dbceaff53c3676dcaa95227cc5816116f32cdbe3
Vrushik <180120116126@git.org.in>
2022-02-01 19:28:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
 
 function App() {
+const [calc,setCalc] = useState("")
+const [result, setResult] = useState('');
+
+
+const ops = ['/','*','-','+','.'];
+
+const updateCalc = value => {
+  setCalc(calc + value)
+}
+
+  const createDigits = () => {
+    const digits = [];
+      for (let i = 0; i < 10; i++) {
+        digits.push(
+          <button onClick={updateCalc()} key={i}>{i}</button>
+        )
+    }
+    return digits;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="calculator">
+        <div className="display">
+          {result ? <span>(0)</span> : ""}
+          {calc || "0" }
+        </div>
+
+        <div className="operators">
+          <button onClick={updateCalc("/")}>/</button>
+          <button onClick={updateCalc("*")}>*</button>
+          <button onClick={updateCalc("+")}>+</button>
+          <button onClick={updateCalc("-")}>-</button>
+
+          <button>DEL</button>
+        </div>
+
+        <div className="digits">
+          {/* <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button> */}
+          {createDigits()}
+          <button onClick={updateCalc("0")}>0</button>
+          <button onClick={updateCalc(".")}>.</button>
+          <button onClick={updateCalc("=")}>=</button>
+        </div>
+        
+      </div>
     </div>
   );
 }
